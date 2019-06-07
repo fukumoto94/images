555754236261ced8ad43a7d49d4bf6d63d5eba6f
lucas <lucas.fukumoto@hotmail.com>
2019-06-07 11:24:37 -0300

    first

---
+++ potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 109400,
+    "boundingBox": {
+        "lx": 437023.1322979051,
+        "ly": 4572671.899073309,
+        "lz": 228.56647479740247,
+        "ux": 437159.11480041497,
+        "uy": 4572807.881575819,
+        "uz": 364.5489773072423
+    },
+    "tightBoundingBox": {
+        "lx": 437023.13,
+        "ly": 4572671.9,
+        "lz": 228.57,
+        "ux": 437144.96,
+        "uy": 4572807.88,
+        "uz": 234.20000000000003
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 0.9421144127845764,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
